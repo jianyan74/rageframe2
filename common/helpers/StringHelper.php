4ac0a94fb338eeae8299fdb3093c535b5bec7c69
hjp1011 <hjp1011@163.com>
2019-09-19 18:24:50 +0800

    update

--- common/helpers/StringHelper.php
+++ common/helpers/StringHelper.php
@@ -420,4 +420,42 @@ class StringHelper extends BaseStringHelper
 
         return $string;
     }
+    /**
+     * 截取字符串
+     *
+     * @param bool $string 字符串
+     * @param int $length 长度
+     * @param int $etc 点位符
+     * @return string
+     */
+    public static function truncate_utf8_string($string, $length, $etc = '...')
+    {
+        $result = '';
+        $string = html_entity_decode(trim(strip_tags($string)), ENT_QUOTES, 'UTF-8');
+        $strlen = strlen($string);
+        for ($i = 0; (($i < $strlen) && ($length > 0)); $i++)
+        {
+            if ($number = strpos(str_pad(decbin(ord(substr($string, $i, 1))), 8, '0', STR_PAD_LEFT), '0'))
+            {
+                if ($length < 1.0)
+                {
+                    break;
+                }
+                $result .= substr($string, $i, $number);
+                $length -= 1.0;
+                $i += $number - 1;
+            }
+            else
+            {
+                $result .= substr($string, $i, 1);
+                $length -= 0.5;
+            }
+        }
+        $result = htmlspecialchars($result, ENT_QUOTES, 'UTF-8');
+        if ($i < $strlen)
+        {
+            $result .= $etc;
+        }
+        return $result;
+    }
 }
