4ca9f1f178473cc5da9dbf057b337614f6b3815d
jianyan74 <panwy@soe-soe.com>
2019-10-16 09:57:38 +0800

    增加phpstorm ide代码提示

---
+++ phpstorm_ide_helper.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * IDE 组件提示,无任何实际功能
+ *
+ * Class Yii
+ */
+class Yii
+{
+    /**
+     * @var MyApplication
+     */
+    public static $app;
+}
+
+/**
+ * Class MyApplication
+ *
+ * @property \yii\redis\Connection $redis
+ * @property \yii\queue\cli\Queue $queue
+ * @property \services\Application $services
+ * @property \common\components\Debris $debris
+ * @property \common\components\Pay $pay
+ * @property \common\components\AddonsModule $addons
+ * @property \addons\RfTinyShop\services\Application $shopServices
+ * @property \Detection\MobileDetect $mobileDetect
+ * @property \jianyan\easywechat\Wechat $wechat
+ * @property \Da\QrCode\Component\QrCodeComponent $qr
+ *
+ * @author jianyan74 <751393839@qq.com>
+ */
+class MyApplication
+{
+
+}
