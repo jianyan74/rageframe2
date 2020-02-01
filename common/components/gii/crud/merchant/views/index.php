d267623a56014d11b6eb5f8a23a8719fb808db6c
uutan <uutan@qq.com>
2020-02-01 18:40:04 +0800

    添加商户端生成模板

---
+++ common/components/gii/crud/merchant/views/index.php
@@ -0,0 +1,98 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\crud\Generator */
+
+$urlParams = $generator->generateUrlParams();
+$nameAttribute = $generator->getNameAttribute();
+
+echo "<?php\n";
+?>
+
+use common\helpers\Html;
+use common\helpers\Url;
+use <?= $generator->indexWidgetType === 'grid' ? "yii\\grid\\GridView" : "yii\\widgets\\ListView" ?>;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = <?= $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>;
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title"><?= "<?= " ?>Html::encode($this->title) ?></h3>
+                <div class="box-tools">
+                    <?= "<?= " ?>Html::create(['edit']) ?>
+                </div>
+            </div>
+            <div class="box-body table-responsive">
+<?php if ($generator->indexWidgetType === 'grid'): ?>
+    <?= "<?= " ?>GridView::widget([
+        'dataProvider' => $dataProvider,
+        'tableOptions' => ['class' => 'table table-hover'],
+        <?= !empty($generator->searchModelClass) ? "'filterModel' => \$searchModel,\n        'columns' => [\n" : "'columns' => [\n"; ?>
+            [
+                'class' => 'yii\grid\SerialColumn',
+                'visible' => false,
+            ],
+
+<?php
+$count = 0;
+if (($tableSchema = $generator->getTableSchema()) === false) {
+    foreach ($generator->getColumnNames() as $name) {
+        if (++$count < 6) {
+            echo "            '" . $name . "',\n";
+        } else {
+            echo "            //'" . $name . "',\n";
+        }
+    }
+} else {
+    $listFields = !empty($generator->listFields) ? $generator->listFields : [];
+    foreach ($tableSchema->columns as $column) {
+        $format = $generator->generateColumnFormat($column);
+        if (in_array($column->name, $listFields)) {
+            echo "            '" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+        } else {
+            echo "            //'" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+        }
+    }
+}
+?>
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'header' => '操作',
+                'template' => '{edit} {status} {delete}',
+                'buttons' => [
+                'edit' => function($url, $model, $key){
+                        return Html::edit(['edit', 'id' => $model->id]);
+                },
+               'status' => function($url, $model, $key){
+                        return Html::status($model['status']);
+                  },
+                'delete' => function($url, $model, $key){
+                        return Html::delete(['delete', 'id' => $model->id]);
+                },
+                ]
+            ]
+    ]
+    ]); ?>
+<?php else: ?>
+    <?= "<?= " ?>ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model-><?= $nameAttribute ?>), ['view', <?= $urlParams ?>]);
+        },
+    ]) ?>
+<?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
